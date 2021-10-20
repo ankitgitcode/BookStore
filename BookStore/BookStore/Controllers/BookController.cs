c523becff88e628633b28edaf0fd47c129317fdb
Ankit <07ankitsingh@gmail.com>
2021-10-20 22:49:46 +0530

    Added a new book controller and its action methods

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+
+        public string GetBook(string id)
+        {
+            if (id == null)
+            {
+                return "No book found";
+            }
+            else
+            return $"book {id}";
+        }
+    }
+}
